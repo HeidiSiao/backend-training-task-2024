ec6e5fb91636f311b291cf1bdb827a0febf8d526
HeidiSiao <heidi.siao@gmail.com>
2025-01-08 00:23:25 +0800

    week1-finished

--- week1/index.js
+++ week1/index.js
@@ -2,12 +2,13 @@
 // 情境：Alex 正在健身房鍛鍊，他正在使用跑步機慢跑，請用變數描述以下狀態：
 
 // 1. 請宣告一個 `const` 變數名稱為 `alexAge`，並賦予值為 `25`。
-// 2. 請宣告一個 `const` 變數名稱為 `alexMembershipID`，並賦予值為字串 `"GYM2024-12345"`。
-// 3. 請宣告一個 `const` 變數名稱為 `isRunningOnTreadmill`，並賦予值為 `true`。
-// 4. 使用 `console.log()` 輸出 `alexAge`、`alexMembershipID` 和 `isRunningOnTreadmill`。
-
-// 1.1 範例
 const alexAge = 25;
+// 2. 請宣告一個 `const` 變數名稱為 `alexMembershipID`，並賦予值為字串 `"GYM2024-12345"`。
+const alexMembershipID = "GYM2024-12345";
+// 3. 請宣告一個 `const` 變數名稱為 `isRunningOnTreadmill`，並賦予值為 `true`。
+const isRunningOnTreadmill = true;
+// 4. 使用 `console.log()` 輸出 `alexAge`、`alexMembershipID` 和 `isRunningOnTreadmill`。
+console.log(alexAge, alexMembershipID, isRunningOnTreadmill);
 
 // ### 題目二：變數命名練習
 // - 瑜伽團課 - 300 元
@@ -16,28 +17,39 @@ const alexAge = 25;
 // 情境：Alex 這個月的運動預算有 3000 元
 // 請修改以下中文變數名稱，讓他符合變數語意
 
-const 變數一 = 300;
-const 變數二 = 500;
-const 變數三 = 1500;
-const 變數四 = 3000;
+const yogaPrice = 300;
+const workoutPrice = 500;
+const single = 1500;
+const totalBudget = 3000;
 
 // ### 題目三：變數計算
 // 呈上題，Alex 想要規劃好自己的運動課程，需符合以下三個條件，請將花費總數計算在 AlexBudget 上，一起幫幫他吧！
 // 條件一：一定至少要買 1 堂重訓團課和瑜伽團課
 // 條件二：瑜伽團課只能一次買 3 堂
 // 條件三：一定要花到 2400 以上，並購買 5 堂課程
+let cost = yogaPrice * 3 + workoutPrice * 2;
+const AlexBudget = totalBudget - cost;
 
 console.log(`Alex 買完課程了，他一共剩下 ${AlexBudget} 元`);
 
 // ### 題目四：線稿圖截圖，看圖宣告變數
 // 請參考資料夾內 q4.webp 圖片
 // 請依照你看到的內容來嘗試設計變數和值（至少 3 個）
+const packageCoursePrice = 2520;
+const coursePrice = 180;
+const discount = 0.9;
 
 // ### 題目五：布林值與變數定義，看是否有用對 const, let
 // 情境：Alex 在往健身房的路上，望向城市的風景，請描述她看到的一切，並宣告變數與賦予值
 // 4-1. Alex 在等紅綠燈，他抬頭看一下現在是紅燈，還有 28 秒綠燈（最多 3 個宣告）
+let isRedLight = true;
+let availableSec = 28;
+
 // 4-2. 目前一起等待的機車有 8 台
+let motorcycle = 8;
 // 4.3. Alex 望向天空，看到天上有 5 朵白雲和 1 顆太陽
+const sunCount = 1;
+let cloudCount = 5;
 
 // ### 題目六：情境題：簡單變數計算
 // 情境：Alex 每天都會帶著 2000cc 的水壺
@@ -50,6 +62,8 @@ console.log(`Alex 買完課程了，他一共剩下 ${AlexBudget} 元`);
 
 let myWater = 2000; // 水壺容量
 myWater -= 500; // 早上喝了 500cc
+let drank = 800 + 700;
+myWater = myWater - drank + 1000;
 
 console.log(`Alex 的水壺還有 ${myWater}cc 的水`);
 
@@ -62,6 +76,11 @@ console.log(`Alex 的水壺還有 ${myWater}cc 的水`);
 
 let totalBill = 0;
 const machineUsePrice = 50;
+const teamCoursePrice = 150;
+let machineUsePriceTotal = machineUsePrice * 3;
+let groupClassesTotal = teamCoursePrice * 2;
+totalBill = machineUsePriceTotal + groupClassesTotal;
+const total = totalBill;
 
 console.log(
   `Anna 本週器械使用費共 ${machineUsePriceTotal} 元，團體課費用共 ${groupClassesTotal} 元，一共消費金額是 ${total}元`
@@ -71,15 +90,15 @@ console.log(
 // 情境：請依照以下程式碼告知答案是多少，並在下方用註解方式寫上這五行程式碼做了什麼事
 // 以下程式碼請勿變更
 let a = 8; // 範例：宣告了一個 a 的變數，並賦予了一個 8 的數字型別
-let b = 0;
-a = 13;
-a = b + 4;
-a - b;
-b += 1;
+let b = 0; // 宣告一個 b 的變數，並賦予了一個 0 的數字型別
+a = 13; // 賦予 a 新值為 13 的數字型別
+a = b + 4; // 答案 a = 4 ; 將 0 + 4 的值 給 a 變數
+a - b; // 將 a值 4 減去 b值 0
+b += 1; //答案 b ＝ 1 ; 將 b值 +1 賦值 給 b 變數
 
 // ### 題目九：型別查詢
 // 請不要觀看 console.log，透過註解告知解答每個變數的型別
-let c = 'world';
+let c = "world";
 let d = 456;
 let e = c + d;
 let f = false;
@@ -87,8 +106,12 @@ let g = d + d;
 let h = f + g;
 
 // 請從以下新增註解，告知上面每行各別是哪些型別
-// a 是 string
-// b 是 ???
+// c 是 string
+// d 是 number
+// e 是 string
+// f 是 boolean
+// g 是 number
+// h 是 number
 
 // ### 題目十：傳值與傳參考
 // 情境：請依照程式碼告知答案是多少，並在下方用註解方式寫上這五行程式碼做了什麼事
@@ -99,3 +122,18 @@ let numberArr2 = numberArr1;
 numberArr2.push(20);
 numberArr2 = [25, 30, 35];
 console.log(numberArr1, numberArr2);
+
+/* 
+輸出：[5,10,15,20],[25, 30, 35]
+
+宣告 numberArr1 為 一個包含 5,10,15 的陣列;
+
+宣告 numberArr2 並 引用參考 numberArr1 即 同個記憶體位置，同為[5, 10, 15];
+
+將 numberArr2 [5,10,15] 使用 push 方法在陣列最後一個位置 加入 20 的值，得到[5,10,15,20]
+並且等同於同時修改了 numberArr1 陣列為 [5,10,15,20];
+
+將 numberArr2 賦予新的值為 [25, 30, 35] 
+
+得到的結果為 console.log([5,10,15,20],[25, 30, 35]);
+*/
